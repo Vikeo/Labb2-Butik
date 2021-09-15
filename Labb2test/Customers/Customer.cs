b8225352d54f504855852d9e18bb03b9bf9e60c1
Viktor Rådberg <66582202+Vikeo@users.noreply.github.com>
2021-09-15 17:18:03 +0200

    Add project files.

---
+++ Labb2test/Customers/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2test.Customers
+{
+    class Customer
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+
+        public Customer(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+        
+    }
+}
---
+++ Labb2test/Program.cs
@@ -0,0 +1,106 @@
+﻿using Labb2test.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Labb2test
+{
+    class Program
+    {
+        private static SessionState _sessionState;
+        private static MenuState _menuState;
+        //public static List<Customer> GetPredefinedUsers()
+        //{
+        //    return new List<Customer>() {  new Customer("Knatte", "123"),
+        //                               new Customer("Fnatte", "321"),
+        //                               new Customer("Tjatte", "213") };
+        //}
+        private static List<Customer> _customers; //initial value
+        
+
+        public List<Customer> Customers
+        {
+            get { return _customers; }
+            set { _customers = value; }
+        }
+
+
+
+
+        enum MenuState
+        {
+            Quit, Welcome, LoggedIn, ShoppingCart
+        }
+        enum SessionState
+        {
+            Active, Inactive
+        }
+
+        static void Main(string[] args)
+        {
+            _menuState = MenuState.Welcome;
+            _sessionState = SessionState.Active;
+
+            //Session active
+            while (_sessionState == SessionState.Active)
+            {
+                var newCustoomer = GenerateCustomer("Knatte", "123");
+                _customers.Add(newCustoomer);
+                
+                Console.WriteLine(_customers.Count);
+                Console.WriteLine(_customers[0].Password);
+                Console.ReadLine();
+                GenerateCustomer("Knatte", "123");
+                Console.WriteLine(_customers.Count);
+                Console.ReadLine();
+                GenerateCustomer("Knatte", "123");
+                Console.WriteLine(_customers.Count);
+                Console.ReadLine();
+                GenerateCustomer("Knatte", "123");
+                Console.WriteLine(_customers.Count);
+                Console.ReadLine();
+
+
+                //Menu1
+                while (_menuState == MenuState.Welcome)
+                {
+                    //RenderWelcomeMenu metod
+                    RenderWelcomeMenu();
+                }
+
+                while (_menuState == MenuState.LoggedIn)
+                {
+                    //RenderLoggedinMenu metod
+                }
+            }
+        }
+
+        private static void RenderWelcomeMenu()
+        {
+            Console.WriteLine("1. Logga in\n2. Registrera användare\n3. Avsluta");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+
+                case "2":
+                    //GenerateCustomer(Console.ReadLine(), Console.ReadLine());
+                    
+                    break;
+
+                case "3":
+                    _sessionState = SessionState.Inactive;
+                    _menuState = MenuState.Quit;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public static Customer GenerateCustomer(string newUsername, string newPassword)//string newUsername, string newPassword
+        {
+            var newCustomer = new Customer(newUsername, newPassword);
+            return newCustomer;
+        }
+    }
+}
